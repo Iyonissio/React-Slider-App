bf3aea116f7e0139b49ecf4b1654c7c11997b996
Iyonissio <iyonissio01@gmail.com>
2021-02-05 11:56:46 -0800

    deploy

---
+++ src/components/SliderData.js
@@ -0,0 +1,16 @@
+
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1511185307590-3c29c11275ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=748&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1515205244153-fce4e5d8bc49?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=821&q=80'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1471771450139-6bfdb4b2609a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=752&q=80'
+    },
+
+]
